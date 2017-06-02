032cbba698dfcbb8d05887401b4c6a32e80c18e1
mineArrow <t.iwai014@gmail.com>
2017-06-02 20:59:49 +0900

    web-infにて動くよう修正

---
+++ aimsoft-attendance/src/main/java/jp/co/aimsoft/attendance/config/WebConfig.java
@@ -0,0 +1,44 @@
+package jp.co.aimsoft.attendance.config;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+import org.thymeleaf.templateresolver.TemplateResolver;
+
+@Configuration
+@EnableAutoConfiguration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+	@Bean
+	public TemplateResolver templateResolver() {
+
+		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver();
+		templateResolver.setPrefix("/WEB-INF/html/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode("HTML5");
+		return templateResolver;
+	}
+
+	@Bean
+	public SpringTemplateEngine templateEngine() {
+
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(templateResolver());
+		return templateEngine;
+	}
+
+	@Bean
+	public ViewResolver viewResolver() {
+
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(templateEngine());
+		viewResolver.setOrder(1);
+		return viewResolver;
+
+	}
+}
