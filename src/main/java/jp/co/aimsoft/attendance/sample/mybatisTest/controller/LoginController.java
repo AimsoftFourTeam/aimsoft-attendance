4444042feba380bc37bd5c2c0ab80f1606c6a314
KotoneTakahashi <di.ocean0323@gmail.com>
2017-07-21 19:17:39 +0900

    ■HTMLのinputタグの記載を修正■HTMLのdata-th-objectを削除■ControllerのリクエストメソッドがGETのModelAndViewの、無駄なform生成を削除■ControllerのリクエストメソッドがPOSTのModelAndViewの引数を整理

--- src/main/java/jp/co/aimsoft/attendance/sample/mybatisTest/controller/LoginController.java
+++ src/main/java/jp/co/aimsoft/attendance/sample/mybatisTest/controller/LoginController.java
@@ -1,9 +1,6 @@
 package jp.co.aimsoft.attendance.sample.mybatisTest.controller;
 
-import javax.validation.Valid;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,13 +14,12 @@ public class LoginController {
 	@RequestMapping(value = "/Login", method = RequestMethod.GET)
 	public ModelAndView FirstDisplay() {
 		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.addObject("form", new LoginForm());
 		modelAndView.setViewName("sample/Login");
 		return modelAndView;
 	}
 
 	@RequestMapping(value = "/LoginCheck", method = RequestMethod.POST)
-	public ModelAndView LoginMenu(@ModelAttribute("form") @Valid LoginForm form, BindingResult result) {
+	public ModelAndView LoginMenu(@ModelAttribute LoginForm form) {
 		ModelAndView LoginMenu = new ModelAndView();
 		LoginMenu.setViewName("sample/EgitHome");
 		LoginMenu.addObject("model", form);
