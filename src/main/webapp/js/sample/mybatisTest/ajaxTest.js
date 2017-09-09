755296b3c2efbe7dfb2c9d0cec90f0086b4fe849
mineArrow <t.iwai014@gmail.com>
2017-09-10 02:58:19 +0900

    正常系はOK。異常系が想定通りにいかず確認中

--- src/main/webapp/js/sample/mybatisTest/ajaxTest.js
+++ src/main/webapp/js/sample/mybatisTest/ajaxTest.js
@@ -1,4 +1,4 @@
-function callbackReplacer(key,value){
+function callbackReplacer(key, value) {
   var formattedString = key + ", " + value;
   return formattedString;
 }
@@ -7,18 +7,11 @@ $(".searchAjax").click(
   function(event) {
     event.preventDefault();
 
-    // 下記では以下のようなデータ構造となる。
-    // "userId=testUser01&userName=&position=&department="
-    // let formJson = $(".main-form").serialize();
-    // ""userId=testUser01&userName=&position=&department=""
-    // var jsonData = JSON.stringify(formJson);
-    // let formJson = $(".main-form").serializeArray();
-    // var jsonData = JSON.stringify(formJson,callbackReplacer);
-
     let $form = $(".main-form");
     var param = {};
+    // コールバック関数にてiにはハッシュKey、vには値（ここでは要素か）が入ってくる。
     $($form.serializeArray()).each(function(i, v) {
-    	param[v.name] = v.value;
+      param[v.name] = v.value;
     });
 
     var json = JSON.stringify(param);
@@ -37,10 +30,14 @@ $(".searchAjax").click(
       dataType: 'json',
       timeout: 100000
     }).done(function(data) {
+      Object.keys(data).forEach(function(key) {
+        var target = document.getElementById(key);
+        if(target !== null){
+          target.value = data[key];
+          // document.getElementById(key).value = data[key];
+        }
+      });
       console.log("ajax成功");
-      $(".userName").val(data["userName"]);
-      $(".position").val(data["position"]);
-      $(".department").val(data["department"]);
     }).fail(function(jqXHR, textStatus, errorThrown) {
       console.log("ajax失敗");
     }).always(function() {
