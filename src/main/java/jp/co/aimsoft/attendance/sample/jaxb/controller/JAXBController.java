9100c76f7bd797225acedf840d0f23c057169f89
mineArrow <t.iwai014@gmail.com>
2017-09-18 03:41:54 +0900

    一旦マージする

--- src/main/java/jp/co/aimsoft/attendance/sample/jaxb/controller/JAXBController.java
+++ src/main/java/jp/co/aimsoft/attendance/sample/jaxb/controller/JAXBController.java
@@ -24,9 +24,9 @@ public class JAXBController {
 	@RequestMapping(value = "/jaxb/sample", method = RequestMethod.GET)
 	public ModelAndView sampleJAXBTest() {
 
-		// File file = new File(
-		// "C:\\Users\\Rigel\\Desktop\\workspace0828-2\\aimsoft-attendance\\src\\main\\java\\jp\\co\\aimsoft\\attendance\\stub\\xml\\sampleStubXxx.xml");
-		File file = new File("sampleStubXxx.xml");
+		// FIXME 相対パス指定方法
+		File file = new File(
+				"C:\\Users\\Rigel\\Desktop\\workspace0828-2\\aimsoft-attendance\\src\\main\\java\\jp\\co\\aimsoft\\attendance\\stub\\xml\\sampleStubXxx.xml");
 
 		SampleStubDtos dtos = JAXB.unmarshal(file, SampleStubDtos.class);
 		dtos.getSampleStubDtoList().removeIf(d -> {
